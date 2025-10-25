9c08eaaf28271e0d1c4d1cff37c9a5acde373b16
marcus <52742540+marcusopiana@users.noreply.github.com>
2025-10-25 04:39:48 +0000

    sudo apt update sudo apt install git-lfs -y git lfs install

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -9,3 +9,7 @@ def home(request):
 def about(request):
     context = {}
     return render(request,'myapp/about.html',context)
+
+# sudo apt update
+# sudo apt install git-lfs -y
+# git lfs install
